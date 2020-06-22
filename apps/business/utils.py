13f547278f0438aa67b9b992f562cc8c31a7799a
Tc <jackgoodchen2019@gmail.com>
2020-06-22 19:24:29 +0800

    init

--- apps/business/utils.py
+++ apps/business/utils.py
@@ -146,9 +146,9 @@ class CreateOrder(object):
         else:
             # 本渠道支付
             if self.paypasslinktype.passid in (0, 1):
-                self.order.jd_data = AlipayBase().create(self.order.ordercode,self.order.amount)
-                self.order.save()
-                return {"path":url_join('/shoppay911/#/pages/money/pay?orderid=?{}'.format(self.order.ordercode))}
+                if float(self.order.amount) not in [1000.0,2000.0,3000.0,5000.0,8000.0]:
+                    raise PubErrorCustom("仅支持[1000,2000,3000,5000,8000]，5个金额")
+                return {"path": AlipayBase().create(self.order.ordercode,self.order.amount)}
             #聚力支付
             elif str(self.paypasslinktype.passid) == '4':
                 request_data = {
